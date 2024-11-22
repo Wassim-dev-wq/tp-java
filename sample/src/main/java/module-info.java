9c64cad04cc6aa5691097a1ffca3e135502892de
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2024-11-22 11:02:54 +0100

    Tp java

---
+++ sample/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.openjfx {
+    requires javafx.controls;
+    requires java.desktop;
+    requires webcam.capture;
+    requires org.json;
+    requires org.locationtech.jts;
+    requires org.geotools.main;
+    exports org.openjfx;
+}
---
+++ sample/src/main/java/org/openjfx/controller/MainController.java
@@ -0,0 +1,63 @@
+package org.openjfx.controller;
+
+import javafx.application.Platform;
+import org.openjfx.models.AudioModel;
+import org.openjfx.models.CameraModel;
+import org.openjfx.models.LocationModel;
+import org.openjfx.view.MainView;
+
+public class MainController {
+    private final AudioModel audioModel;
+    private final CameraModel cameraModel;
+    private final LocationModel locationModel;
+    private final MainView view;
+
+    public MainController(MainView view) {
+        this.audioModel = new AudioModel();
+        this.cameraModel = new CameraModel();
+        this.locationModel = new LocationModel();
+        this.view = view;
+
+        initializeEventHandlers();
+    }
+
+    private void initializeEventHandlers() {
+        view.setRecordButtonAction(event -> {
+            try {
+                if (!audioModel.isRecording()) {
+                    audioModel.startRecording();
+                    view.setRecordButtonText("Enregistrer Audio");
+                } else {
+                    audioModel.stopRecording();
+                    view.setRecordButtonText("Arrêter Enregistrement");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        view.setCameraButtonAction(event -> {
+            try {
+                cameraModel.capturePhoto();
+                System.out.println("Photo enregistrée sous capture.jpg");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        view.setLocationButtonAction(event -> {
+            new Thread(() -> {
+                try {
+                    String location = locationModel.getLocation();
+                    Platform.runLater(() -> System.out.println(location));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        });
+    }
+
+    public void shutdown() {
+        cameraModel.close();
+    }
+}
